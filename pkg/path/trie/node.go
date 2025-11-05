55d6098832f60a455ead2272acc9ffe2652feb25
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-11-05 10:18:57 -0600

    Missed that

--- pkg/path/trie/node.go
+++ pkg/path/trie/node.go
@@ -17,7 +17,7 @@ func (n Node[K]) IsLeaf() bool {
 	return len(n) == 0
 }
 
-func (n Node[K]) Lookup(path string) (Node[K], bool) {
+func (n Node[K]) Lookup(path K) (Node[K], bool) {
 	node, found := n, 0
 	for node != nil && !node.IsLeaf() && found < len(path) {
 		node, found = node.next(path, found)
@@ -26,7 +26,7 @@ func (n Node[K]) Lookup(path string) (Node[K], bool) {
 	return node, node != nil && node.IsLeaf() && found == len(path)
 }
 
-func (n Node[K]) next(path string, found int) (Node[K], int) {
+func (n Node[K]) next(path K, found int) (Node[K], int) {
 	for _, edge := range n {
 		if hasPrefix(path[found:], edge.Label) {
 			return edge.Target, found + len(edge.Label)
@@ -36,6 +36,6 @@ func (n Node[K]) next(path string, found int) (Node[K], int) {
 	return nil, 0
 }
 
-func hasPrefix[K Key](path string, prefix K) bool {
-	return strings.HasPrefix(path, string(prefix))
+func hasPrefix[K Key](path K, prefix K) bool {
+	return strings.HasPrefix(string(path), string(prefix))
 }
