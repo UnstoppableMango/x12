4d1756e3efa654faa5239f26ce737a4df4c78190
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-01-16 13:08:42 -0600

    Result package (#7)
    * WIP
    
    * Result package

---
+++ pkg/result/result.go
@@ -0,0 +1,38 @@
+package result
+
+import (
+	"github.com/unstoppablemango/x12/pkg/app"
+)
+
+type Path = app.Path
+
+type Request[T any] interface {
+	app.Request
+
+	SetResult(T)
+	SetError(error)
+}
+
+type Handler[Res any, Req Request[Res]] interface {
+	Handle(Req) (Res, error)
+}
+
+type HandlerFunc[Res any, Req Request[Res]] func(Req) (Res, error)
+
+func (handle HandlerFunc[Res, Req]) Handle(req Req) (Res, error) {
+	return handle(req)
+}
+
+func Handle[Res any, Req Request[Res]](path Path, handler Handler[Res, Req]) app.Option[Req] {
+	return app.HandleFunc(path, func(req Req) {
+		if res, err := handler.Handle(req); err != nil {
+			req.SetError(err)
+		} else {
+			req.SetResult(res)
+		}
+	})
+}
+
+func HandleFunc[Res any, Req Request[Res]](path Path, handler func(Req) (Res, error)) app.Option[Req] {
+	return Handle(path, HandlerFunc[Res, Req](handler))
+}
